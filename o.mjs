5519ef9225dd43e3048c50c546deb37d5df2b852
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-11-01 15:29:38 +0100

    initial commit, even with some tests

---
+++ o.mjs
@@ -0,0 +1,140 @@
+// Hyperscript function
+export const h = (e, p = {}, ...c) => ({ e, p, c });
+
+// Minimal JSX-like parser based on tagged template literals
+export const x = (strings, ...fields) => {
+  const stack = [{ c: [] }];
+  const find = (s, re, arg) => {
+    if (!s) {
+      return [s, arg];
+    }
+    let m = s.match(re);
+    return [s.substring(m[0].length), m[1]];
+  };
+  const MODE_TEXT = 0;
+  const MODE_OPEN = 1;
+  const MODE_CLOSE = 2;
+  let mode = MODE_TEXT;
+  strings.forEach((s, i) => {
+    while (s) {
+      let val;
+      s = s.trimLeft();
+      switch (mode) {
+        case MODE_TEXT:
+          if (s[0] === '<') {
+            if (s[1] === '/') {
+              [s, val] = find(s.substring(2), /^([a-zA-Z]+)/, fields[i]);
+              mode = MODE_CLOSE;
+            } else {
+              [s, val] = find(s.substring(1), /^([a-zA-Z]+)/, fields[i]);
+              mode = MODE_OPEN;
+              stack.push({ e: val, p: {}, c: [] });
+            }
+          } else {
+            [s, val] = find(s, /^([^<]+)/, '');
+            stack[stack.length - 1].c.push(val);
+          }
+          break;
+        case MODE_OPEN:
+          if (s[0] === '/' && s[1] === '>') {
+            s = s.substring(2);
+            stack[stack.length - 2].c.push(stack.pop());
+            mode = MODE_TEXT;
+          } else if (s[0] === '>') {
+            s = s.substring(1);
+            mode = MODE_TEXT;
+          } else {
+            let m = s.match(/^([a-zA-Z0-9]+)=/);
+            console.assert(m);
+            s = s.substring(m[0].length);
+            let k = m[1];
+            [s, val] = find(s, /^"([^"]*)"/, fields[i]);
+            stack[stack.length - 1].p[k] = val;
+          }
+          break;
+        case MODE_CLOSE:
+          console.assert(s[0] === '>');
+          stack[stack.length - 2].c.push(stack.pop());
+          s = s.substring(1);
+          mode = MODE_TEXT;
+          break;
+      }
+    }
+    if (mode === MODE_TEXT) {
+      stack[stack.length - 1].c = stack[stack.length - 1].c.concat(fields[i]);
+    }
+  });
+  return stack[0].c[0];
+};
+
+let hooks;
+let index = 0;
+let forceUpdate;
+let getHook = init => {
+  let hook = hooks[index++];
+  if (!hook) {
+    hook = { value: init };
+    hooks.push(hook);
+  }
+  return hook;
+};
+
+export const useReducer = (reducer, init) => {
+  const hook = getHook(init);
+  const f = forceUpdate;
+  const dispatch = v => {
+    hook.value = reducer(hook.value, v);
+    f();
+  };
+  return [hook.value, dispatch];
+};
+
+export const useState = init => useReducer((_, v) => v, init);
+
+// Patch DOM according to the hyperscript nodes
+export const render = (vlist, dom) => {
+  // Make vlist always an array, even if it's a single node.
+  vlist = [].concat(vlist);
+  // Unique implicit keys counter for un-keyed nodes
+  let ids = {};
+  // Current hooks storage
+  let hs = dom.h || {};
+  // Erase hooks storage
+  dom.h = {};
+  vlist.forEach((v, i) => {
+    // Current component re-rendering function (global, used by some hooks).
+    forceUpdate = () => render(vlist, dom);
+    while (typeof v.e === 'function') {
+      // Key, explicit v property or implicit auto-incremented key
+      let k = (v.p && v.p.k) || '' + v.e + (ids[v.e] = (ids[v.e] || 1) + 1);
+      hooks = hs[k] || [];
+      index = 0;
+      v = v.e(v.p, v.c, forceUpdate);
+      // Put current hooks into the new hooks storage
+      dom.h[k] = hooks;
+    }
+    // DOM node builder for the given v node
+    let $ = () =>
+      v.e ? document.createElement(v.e) : document.createTextNode(v);
+    // Corresponding DOM node, if any. Reuse if tag and text matches. Insert
+    // new DOM node before otherwise.
+    let n = dom.childNodes[i];
+    if (!n || (n.e !== v.e && n.data !== v)) {
+      n = dom.insertBefore($(), n);
+    }
+    if (v.e) {
+      n.e = v.e;
+      for (let k in v.p) {
+        if (n[k] !== v.p[k]) {
+          n[k] = v.p[k];
+        }
+      }
+      render(v.c, n);
+    } else {
+      n.data = v;
+    }
+  });
+  for (let c; (c = dom.childNodes[vlist.length]); ) {
+    dom.removeChild(c);
+  }
+};
---
+++ test.mjs
@@ -0,0 +1,208 @@
+import assert from 'assert';
+import jsdom from 'jsdom';
+
+import { h, x, render, useState, useReducer } from './o.mjs';
+
+// Global document mock
+global.document = new jsdom.JSDOM(`<html><body></body></html>`).window.document;
+
+// Global map of tests
+const $ = {};
+
+// ---------------------------------------------------------------------------
+// Hyperscript function should have default properties as an object,
+// and default children list as an array.
+// ---------------------------------------------------------------------------
+$['hyperscript'] = () => {
+  assert.deepEqual(h('div'), { e: 'div', p: {}, c: [] });
+  assert.deepEqual(h('div', undefined, 'Hello'), {
+    e: 'div',
+    p: {},
+    c: ['Hello']
+  });
+  assert.deepEqual(h('div', { a: 1 }, 'Hello'), {
+    e: 'div',
+    p: { a: 1 },
+    c: ['Hello']
+  });
+};
+
+// ---------------------------------------------------------------------------
+// Syntax sugar with tagged template literals
+// ---------------------------------------------------------------------------
+$['x: empty'] = () => assert.equal(x``, undefined);
+$['x: text'] = () => assert.equal(x`hello`, 'hello');
+$['x: normal tag'] = () => assert.deepEqual(x`<div></div>`, h('div'));
+$['x: self-closing tag'] = () => assert.deepEqual(x`<br />`, h('br'));
+$['x: attrs'] = () =>
+  assert.deepEqual(x`<div a="b" c="d e" />`, h('div', { a: 'b', c: 'd e' }));
+$['x: tag with text'] = () =>
+  assert.deepEqual(x`<p>Hello</p>`, h('p', {}, 'Hello'));
+$['x: nested tags'] = () =>
+  assert.deepEqual(x`<p><i>Hello</i></p>`, h('p', {}, h('i', {}, 'Hello')));
+$['x: ${text}'] = () => {
+  assert.deepEqual(x`<p>hello, ${'world'}</p>`, h('p', {}, 'hello, ', 'world'));
+  assert.deepEqual(
+    x`<p>hello, ${'world'}!</p>`,
+    h('p', {}, 'hello, ', 'world', '!')
+  );
+  assert.deepEqual(
+    x`<p>${'world'}, hello!</p>`,
+    h('p', {}, 'world', ', hello!')
+  );
+};
+$['x: ${tag}'] = () => {
+  assert.deepEqual(x`<${'p'}>hello</${'p'}>`, h('p', {}, 'hello'));
+  assert.deepEqual(x`<${'br'} />`, h('br'));
+};
+$['x: ${attr}'] = () => {
+  assert.deepEqual(x`<p a=${'b'} c=${'d'}/>`, h('p', { a: 'b', c: 'd' }));
+};
+
+// ---------------------------------------------------------------------------
+// Simple rendering of stateless components with hyperscript
+// ---------------------------------------------------------------------------
+$['render: single stateless node'] = () => {
+  render(x`<div className="simple">text</div>`, document.body);
+  assert.equal(document.querySelector('.simple').textContent, 'text');
+};
+$['render: single stateless component'] = () => {
+  const Hello = () => x`<div className="component">Hello</div>`;
+  render(h(Hello), document.body);
+  assert.equal(document.querySelector('.component').textContent, 'Hello');
+};
+$['render: component with properties'] = () => {
+  const Text = ({ cls, text }) => x`<p className=${cls}>${text}</p>`;
+  render(h(Text, { cls: 'foo', text: 'bar' }), document.body);
+  assert.equal(document.querySelector('.foo').textContent, 'bar');
+};
+$['render: component with children'] = () => {
+  const A = () => x`<div className="a">A</div>`;
+  const B = () => x`<div className="b">B</div>`;
+  const C = (props, children) => x`<div className="c">${children}</div>`;
+  const D = () => x`<${C}><${C}><${A} /><${B} /></${C}></${C}>`;
+  render(h(D), document.body);
+  assert.equal(document.querySelector('.c > .c > .a').textContent, 'A');
+  assert.equal(document.querySelector('.c > .c > .b').textContent, 'B');
+};
+
+// ---------------------------------------------------------------------------
+// forceUpdate()
+// ---------------------------------------------------------------------------
+$['forceUpdate: click counter'] = () => {
+  let n = 0;
+  const Counter = (props, children, forceUpdate) => {
+    const handleClick = () => {
+      n++;
+      forceUpdate();
+    };
+    return x`
+      <div>
+        <div className="count">Count: ${n}</div>
+        <button onclick=${handleClick}>Add</button>
+      </div>
+    `;
+  };
+  render(h(Counter), document.body);
+  assert.equal(document.querySelector('.count').textContent, 'Count: 0');
+  document.querySelector('button').onclick();
+  assert.equal(document.querySelector('.count').textContent, 'Count: 1');
+  document.querySelector('button').onclick();
+  assert.equal(document.querySelector('.count').textContent, 'Count: 2');
+};
+
+// ---------------------------------------------------------------------------
+// Hooks
+// ---------------------------------------------------------------------------
+$['hooks: useState'] = () => {
+  const Counter = () => {
+    const [n, setN] = useState(0);
+    return x`
+      <div>
+        <div className="count">Count: ${n}</div>
+        <button onclick=${() => setN(n + 1)}>Add</button>
+      </div>
+    `;
+  };
+  document.body.innerHTML = ''; // clear all the previous hooks
+  render(h(Counter), document.body);
+  assert.equal(document.querySelector('.count').textContent, 'Count: 0');
+  document.querySelector('button').onclick();
+  assert.equal(document.querySelector('.count').textContent, 'Count: 1');
+  document.querySelector('button').onclick();
+  assert.equal(document.querySelector('.count').textContent, 'Count: 2');
+};
+
+$['hooks: useReducer'] = () => {
+  const reducer = (state, action) => {
+    switch (action) {
+      case 'incr':
+        return state + 1;
+      case 'decr':
+        return state - 1;
+    }
+    return state;
+  };
+  const Counter = () => {
+    const [n, dispatch] = useReducer(reducer, 0);
+    return x`
+      <div>
+        <div className="count">Count: ${n}</div>
+        <button onclick=${() => dispatch('incr')}>Add</button>
+      </div>
+    `;
+  };
+  document.body.innerHTML = ''; // clear all the previous hooks
+  render(h(Counter), document.body);
+  assert.equal(document.querySelector('.count').textContent, 'Count: 0');
+  document.querySelector('button').onclick();
+  assert.equal(document.querySelector('.count').textContent, 'Count: 1');
+  document.querySelector('button').onclick();
+  assert.equal(document.querySelector('.count').textContent, 'Count: 2');
+};
+
+$['hooks: reorder with id'] = () => {
+  const C = ({ id }) => {
+    const [n, setN] = useState(0);
+    return x`
+      <div id=${id}>
+        <div className="count">Count: ${n}</div>
+        <button onclick=${() => setN(n + 1)}>Add</button>
+      </div>
+    `;
+  };
+  const A = () =>
+    x`<div><${C} id="c1" k="c1"/><${C} id="c2" k="c2"/><br/></div>`;
+  const B = () =>
+    x`<div><br/><${C} id="c3" k="c2"/><${C} id="c4" k="c1"/></div>`;
+  document.body.innerHTML = ''; // clear all the previous hooks
+  render(h(A), document.body);
+  document.querySelector('#c1 button').onclick();
+  document.querySelector('#c2 button').onclick();
+  document.querySelector('#c2 button').onclick();
+  assert.equal(document.querySelector('#c1 .count').textContent, 'Count: 1');
+  assert.equal(document.querySelector('#c2 .count').textContent, 'Count: 2');
+  render(h(B), document.body);
+  assert.equal(document.querySelector('#c3 .count').textContent, 'Count: 2');
+  assert.equal(document.querySelector('#c4 .count').textContent, 'Count: 1');
+};
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+// Try running "+" tests only, otherwise run all tests, skipping "-" tests.
+if (
+  Object.keys($)
+    .filter(t => t.startsWith('+'))
+    .map(t => $[t]()).length == 0
+) {
+  for (let t in $) {
+    if (t.startsWith('-')) {
+      console.log('SKIP:', t);
+    } else {
+      console.log('TEST:', t);
+      $[t]();
+    }
+  }
+}
